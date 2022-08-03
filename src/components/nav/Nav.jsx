a3c64d94cbfcdd06ed5fc35956f3148d3ab73d7d
mrSandeepKr <sandeep.kumar.ece16@itbhu.ac.in>
2022-08-04 01:58:06 +0530

    configure navigation

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import './nav.css'
+import {AiOutlineHome, AiOutlineUser} from 'react-icons/ai'
+import {BiBook, BiMessageSquareDetail} from 'react-icons/bi'
+import {RiServiceLine} from 'react-icons/ri'
+import {useState} from 'react'
 
 const nav = () => {
+
+  const [activeNav, setActiveNacv] = useState('#')
+  
   return (
-    <div>nav</div>
+    <nav>
+      <a href='#' onClick= {() => {setActiveNacv('#')}} className= {activeNav === '#' ? 'active' : ''}><AiOutlineHome/></a>
+      <a href='#about' onClick= {() => {setActiveNacv('#about')}} className= {activeNav === '#about' ? 'active' : ''}><AiOutlineUser/></a>
+      <a href='#experience' onClick= {() => {setActiveNacv('#experience')}} className= {activeNav === '#experience' ? 'active' : ''}> <BiBook/></a>
+      <a href='#services' onClick= {() => {setActiveNacv('#services')}} className= {activeNav === '#services' ? 'active' : ''}> <RiServiceLine/></a>     
+      <a href='#contact' onClick= {() => {setActiveNacv('#contact')}} className= {activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail/></a>
+    </nav>
   )
 }
 

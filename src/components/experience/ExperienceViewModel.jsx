9a2dc08b7c73e5a3f5c6502f7bbf71bf0bf2061c
mrSandeepKr <sandeep.kumar.ece16@itbhu.ac.in>
2022-08-05 18:21:01 +0530

    adding experience component

---
+++ src/components/experience/ExperienceViewModel.jsx
@@ -0,0 +1,62 @@
+let viewModel = {
+    frontEndData: {
+        title: 'Frontend Development',
+        skillLevelList: [
+            {
+                title: 'iOS',
+                level: 'Higher Intermediate'
+            },
+            {
+                title: 'HTML',
+                level: 'Intermediate'
+            },
+            {
+                title: 'CSS',
+                level: 'Intermediate'
+            },
+            {
+                title: 'XCode',
+                level: 'Intermediate'
+            },
+            {
+                title: 'DS & Algo',
+                level: 'Expert'
+            },
+            {
+                title: 'Cpp',
+                level: 'Higher Intermediate'
+            },
+            {
+                title: 'Javascript',
+                level: 'Beginner'
+            }
+        ]
+    },
+    backEndData: {
+        title: 'Backend Development',
+        skillLevelList: [
+            {
+                title: 'DotNet',
+                level: 'Beginner'
+            },
+            {
+                title: 'MongoDB',
+                level: 'Beginner'
+            },
+            {
+                title: 'Microservices',
+                level: 'Beginner'
+            },
+            {
+                title: 'Docker',
+                level: 'Beginner'
+            },
+            {
+                title: 'Kubernetes',
+                level: 'Beginner'
+            }
+        ]
+    }
+}
+
+export default viewModel

8b06f5fdd40f123778f1e62d0765ec58b181f930
mrSandeepKr <sandeep.kumar.ece16@itbhu.ac.in>
2022-08-06 22:45:22 +0530

    adding services

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import './services.css'
+import ServicesViewModel from './ServicesViewModel'
+import ServiceElement from './ServiceElement'
 
 const Services = () => {
+  let services = ServicesViewModel.map((se) => {return <ServiceElement model = {se}/>})
+
   return (
     <section id='services'>
-      Services
+      <h5>What I offer?</h5>
+      <h2>Services</h2>
+
+      <div className="container services__container">
+        {services}
+      </div>
     </section>
   )
 }

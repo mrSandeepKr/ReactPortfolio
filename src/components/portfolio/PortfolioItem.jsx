eca37eafb58cfe642db46caf0d56418dd5ee3831
mrSandeepKr <sandeep.kumar.ece16@itbhu.ac.in>
2022-08-07 13:20:14 +0530

    adding swiper to portfolio component

--- src/components/portfolio/PortfolioItem.jsx
+++ src/components/portfolio/PortfolioItem.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 
+// Swiper Imports
+import { Navigation, Pagination, Autoplay} from 'swiper'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/swiper-bundle.min.css'
+import 'swiper/swiper.min.css'
+
 const PortfolioItem = (
     model
 ) => {
-    model = model.model
+    model = model.model;
 
     return (
         <article className='portfolio__item'>
-            <div className='porfolio__item-image'>
-                <img src = {model.imageURL}  alt = {model.title}/>
+            <div className='porfolio__item-display'>
+                <PortfolioGraphicDetail imageURLs = {model.imageURLs} />
             </div>
 
             <div className='portfolio__item-details'>
@@ -16,6 +22,35 @@ const PortfolioItem = (
                 <a href={model.projectURL} className='btn' target ='_blank'>LINK</a>
             </div>
         </article>
+    )
+}
+
+const PortfolioGraphicDetail = (
+    imageURLs
+) => {
+    imageURLs = imageURLs.imageURLs
+    if(imageURLs.length == 1) {
+        return (
+            <img src = {imageURLs[0]}  alt = {imageURLs[0]}/>
+        )
+    }
+    
+    let slides = imageURLs.map((imageURL, index) => { return (
+        <SwiperSlide key = {index}> 
+            <img src = {imageURL}  alt = {imageURL}/> 
+        </SwiperSlide>
+        )
+    })
+
+    return (
+        <Swiper
+        modules = {[ Pagination, Autoplay ]}
+        spaceBetween = {40}
+        slidesPerView = {1}
+        pagination={{ clickable: true }}
+        autoplay= {{delay: 1500, pauseOnMouseEnter: true, disableOnInteraction: false}}>
+            {slides}
+        </Swiper>
     )
 }
 
--- src/components/portfolio/PortfolioViewModel.jsx
+++ src/components/portfolio/PortfolioViewModel.jsx
@@ -1,22 +1,31 @@
 let PortfolioViewModel = [
     {
         title: 'Safe Messenger',
-        imageURL: 'https://github.com/mrSandeepKr/SafeMessenger/blob/master/ReadMeImages/ChatList.png?raw=true',
+        imageURLs: [
+            'https://github.com/mrSandeepKr/SafeMessenger/blob/master/ReadMeImages/ChatList.png?raw=true',
+            'https://github.com/mrSandeepKr/SafeMessenger/blob/master/ReadMeImages/ChatExp2.png?raw=true'
+        ],
         projectURL: 'https://github.com/mrSandeepKr/SafeMessenger'
     },
     {
         title: 'Cowin Vaccine Status',
-        imageURL: 'https://github.com/mrSandeepKr/Medium-CowinVaccineStatusProj/blob/main/ReadMeImages/Screenshot%202021-09-12%20at%2012.48.40%20AM.png?raw=true',
+        imageURLs: [
+            'https://github.com/mrSandeepKr/Medium-CowinVaccineStatusProj/blob/main/ReadMeImages/Screenshot%202021-09-12%20at%2012.48.40%20AM.png?raw=true'
+        ],
         projectURL: 'https://github.com/mrSandeepKr/Medium-CowinVaccineStatusProj'
     },
     {
         title: 'Spotify Clone',
-        imageURL: 'https://github.com/mrSandeepKr/Spotify-Clone/blob/master/ReadMeImages/PlaylistView.png?raw=true',
+        imageURLs: [
+            'https://github.com/mrSandeepKr/Spotify-Clone/blob/master/ReadMeImages/PlaylistView.png?raw=true'
+        ],
         projectURL: 'https://github.com/mrSandeepKr/Spotify-Clone'
     },
     {
         title: 'Custom App Testing Script',
-        imageURL: 'https://miro.medium.com/max/1092/0*xxMMKqpxI8jPEn2m',
+        imageURLs: [
+            'https://miro.medium.com/max/1092/0*xxMMKqpxI8jPEn2m'
+        ],
         projectURL: 'https://github.com/mrSandeepKr/ScriptToAuomateActionAppDataGeneration'
     }
 ]

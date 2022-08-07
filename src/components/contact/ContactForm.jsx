18ac02418f5276bb9a6809aa2a046e0b14eb15e7
mrSandeepKr <sandeep.kumar.ece16@itbhu.ac.in>
2022-08-07 20:29:02 +0530

    integrating emailJS

--- src/components/contact/ContactForm.jsx
+++ src/components/contact/ContactForm.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, {useRef} from 'react'
+import emailjs from 'emailjs-com'
 
 const ContactForm = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs.init('{Public Key}');
+    emailjs.sendForm('{Service ID}', '{template ID}', form.current)
+  };
+
   return (
-    <form className="contact__form" action = "">
+    <form className="contact__form" ref = {form} onSubmit={sendEmail}>
       <input type = 'text' name = 'name' placeholder='Fill Your Name' required />
       <input type = 'email' name = 'email' placeholder='Your Email' required />
       <textarea name = 'message' rows = '7' placeholder='Your Message' required></textarea>
